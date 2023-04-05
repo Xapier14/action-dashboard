5b6f0835e0fd05a4584a436a389d1d0b2d7273ff
Lance Crisang <lancecrisang14@gmail.com>
2023-04-05 15:50:55 +0800

    Add Inspector section

--- src/app/viewer/view-incident/view-incident.component.ts
+++ src/app/viewer/view-incident/view-incident.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AccountData, AccountsService } from 'src/app/services/accounts.service';
+import { FullReportData, ReportsService } from 'src/app/services/reports.service';
 
 @Component({
   selector: 'app-view-incident',
@@ -9,13 +11,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ViewIncidentComponent {
   id: string = '';
+  reportData: FullReportData | null = null;
+  inspectorData: AccountData | null = null;
+  error: string = '';
   constructor(private route: ActivatedRoute,
-              private title: Title) {}
+              private router: Router,
+              private title: Title,
+              private reportsService: ReportsService,
+              private accountsService: AccountsService) {}
 
   ngOnInit() {
-    this.title.setTitle('Report Viewer - ACTION Dashboard Web App');
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe(async params => {
       this.id = params['id'];
+      this.reportData = await this.reportsService.tryGetReportAsync(this.id);
+      if (!this.reportData) {
+        this.router.navigate(['errors', 'notfound']);
+        return;
+      }
+      console.log(this.reportData);
+      this.inspectorData = await this.accountsService.getAccountDataAsync(this.reportData?.inspectorId ?? '');
+      if (!this.inspectorData) {
+        this.error = 'Inspector user data could not be retrieved';
+        return;
+      }
+      this.title.setTitle('Report Viewer - ACTION Dashboard Web App');
     })
   }
 }
