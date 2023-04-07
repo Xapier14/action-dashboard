ef4f9e3ea4708735112da5a8c11593ecd7f1daa6
Lance Crisang <lancecrisang14@gmail.com>
2023-04-07 13:17:11 +0800

    Add evaluation section

--- src/app/viewer/view-incident/view-incident.component.ts
+++ src/app/viewer/view-incident/view-incident.component.ts
@@ -4,6 +4,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AccountData, AccountsService } from 'src/app/services/accounts.service';
 import { FullReportData, ReportsService } from 'src/app/services/reports.service';
 
+interface ObservationData {
+  label: string;
+  optionalLabel: string;
+  value: number;
+}
+
 @Component({
   selector: 'app-view-incident',
   templateUrl: './view-incident.component.html',
@@ -13,6 +19,7 @@ export class ViewIncidentComponent {
   id: string = '';
   reportData: FullReportData | null = null;
   inspectorData: AccountData | null = null;
+  observationData: ObservationData[] | null = null;
   error: string = '';
   constructor(private route: ActivatedRoute,
               private router: Router,
@@ -33,8 +40,50 @@ export class ViewIncidentComponent {
         this.error = 'Inspector user data could not be retrieved';
         return;
       }
+      this.observationData = this.generateObservationData(this.reportData);
       this.title.setTitle('Report Viewer - ACTION Dashboard Web App');
     })
+  }
+
+  generateObservationData(reportData: FullReportData): ObservationData[] {
+    const data: ObservationData[] = [
+      {
+        label: 'Collapsed Structure',
+        optionalLabel : '',
+        value: reportData.collapsedStructure
+      },
+      {
+        label: 'Building or story leaning / out of plumb',
+        optionalLabel : '',
+        value: reportData.leaningOrOutOfPlumb
+      },
+      {
+        label: 'Damage to primary structural members, racking of walls',
+        optionalLabel : '',
+        value: reportData.damageToPrimaryStructure
+      },
+      {
+        label: 'Falling hazards such',
+        optionalLabel : '',
+        value: reportData.fallingHazards
+      },
+      {
+        label: 'Ground movement or slope',
+        optionalLabel : '',
+        value: reportData.groundMovementOrSlope
+      },
+      {
+        label: 'Damaged / submerged fixtures or services',
+        optionalLabel : '',
+        value: reportData.damagedSubmergedFixtures,
+      },
+      {
+        label: 'Proximity risks / other (specify): ',
+        optionalLabel : reportData.proximityRiskTitle,
+        value: reportData.proximityRisk
+      }
+    ];
+    return data;
   }
 
   formatDateToShortDateString(date: Date): string {
