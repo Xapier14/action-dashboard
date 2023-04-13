87646b0568167e74e43dcff2d398db245bd5c701
Lance Crisang <lancecrisang14@gmail.com>
2023-04-14 00:52:07 +0800

    Add posting section

--- src/app/viewer/view-incident/view-incident.component.ts
+++ src/app/viewer/view-incident/view-incident.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AccountData, AccountsService } from 'src/app/services/accounts.service';
-import { FullReportData, ReportsService } from 'src/app/services/reports.service';
+import {
+  AccountData,
+  AccountsService,
+} from 'src/app/services/accounts.service';
+import {
+  FullReportData,
+  ReportsService,
+} from 'src/app/services/reports.service';
 
 interface ObservationData {
   label: string;
@@ -13,7 +19,7 @@ interface ObservationData {
 @Component({
   selector: 'app-view-incident',
   templateUrl: './view-incident.component.html',
-  styleUrls: ['./view-incident.component.scss']
+  styleUrls: ['./view-incident.component.scss'],
 })
 export class ViewIncidentComponent {
   id: string = '';
@@ -21,80 +27,88 @@ export class ViewIncidentComponent {
   inspectorData: AccountData | null = null;
   observationData: ObservationData[] | null = null;
   error: string = '';
-  constructor(private route: ActivatedRoute,
-              private router: Router,
-              private title: Title,
-              private reportsService: ReportsService,
-              private accountsService: AccountsService) {}
+  currentLoading: string = 'Initializing...';
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private title: Title,
+    private reportsService: ReportsService,
+    private accountsService: AccountsService
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(async params => {
+    this.route.params.subscribe(async (params) => {
       this.id = params['id'];
+      this.currentLoading = 'Retrieving report data...';
       this.reportData = await this.reportsService.tryGetReportAsync(this.id);
       if (!this.reportData) {
         this.router.navigate(['errors', 'notfound']);
         return;
       }
-      this.inspectorData = await this.accountsService.getAccountDataAsync(this.reportData?.inspectorId ?? '');
+      this.currentLoading = 'Retrieving inspector data...';
+      this.inspectorData = await this.accountsService.getAccountDataAsync(
+        this.reportData?.inspectorId ?? ''
+      );
       if (!this.inspectorData) {
         this.error = 'Inspector user data could not be retrieved';
         return;
       }
+      this.currentLoading = 'Generating observation data...';
       this.observationData = this.generateObservationData(this.reportData);
       this.title.setTitle('Report Viewer - ACTION Dashboard Web App');
-    })
+    });
   }
 
   generateObservationData(reportData: FullReportData): ObservationData[] {
     const data: ObservationData[] = [
       {
         label: 'Collapsed Structure',
-        optionalLabel : '',
-        value: reportData.collapsedStructure
+        optionalLabel: '',
+        value: reportData.collapsedStructure,
       },
       {
         label: 'Building or story leaning / out of plumb',
-        optionalLabel : '',
-        value: reportData.leaningOrOutOfPlumb
+        optionalLabel: '',
+        value: reportData.leaningOrOutOfPlumb,
       },
       {
         label: 'Damage to primary structural members, racking of walls',
-        optionalLabel : '',
-        value: reportData.damageToPrimaryStructure
+        optionalLabel: '',
+        value: reportData.damageToPrimaryStructure,
       },
       {
         label: 'Falling hazards such',
-        optionalLabel : '',
-        value: reportData.fallingHazards
+        optionalLabel: '',
+        value: reportData.fallingHazards,
       },
       {
         label: 'Ground movement or slope',
-        optionalLabel : '',
-        value: reportData.groundMovementOrSlope
+        optionalLabel: '',
+        value: reportData.groundMovementOrSlope,
       },
       {
         label: 'Damaged / submerged fixtures or services',
-        optionalLabel : '',
+        optionalLabel: '',
         value: reportData.damagedSubmergedFixtures,
       },
       {
         label: 'Proximity risks / other (specify): ',
-        optionalLabel : reportData.proximityRiskTitle,
-        value: reportData.proximityRisk
-      }
+        optionalLabel: reportData.proximityRiskTitle,
+        value: reportData.proximityRisk,
+      },
     ];
     return data;
   }
 
   formatDateToShortDateString(date: Date): string {
     const oY: Intl.DateTimeFormatOptions = {
-      year: '2-digit'
+      year: '2-digit',
     };
     const oM: Intl.DateTimeFormatOptions = {
-      month: 'short'
+      month: 'short',
     };
     const oD: Intl.DateTimeFormatOptions = {
-      day: '2-digit'
+      day: '2-digit',
     };
     const y = date.toLocaleDateString('en-US', oY);
     const m = date.toLocaleDateString('en-US', oM).toUpperCase();
@@ -104,11 +118,9 @@ export class ViewIncidentComponent {
 
   formatDateTo24TimeString(date: Date): string {
     var h = date.getHours().toString();
-    if (h.length == 1)
-      h = '0' + h;
+    if (h.length == 1) h = '0' + h;
     var m = date.getMinutes().toString();
-    if (m.length == 1)
-      m = '0' + m;
+    if (m.length == 1) m = '0' + m;
     return `${h}:${m}`;
   }
 }
