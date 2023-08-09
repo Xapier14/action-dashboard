52a50ed01d864194d17f9b6fb6be10257ab14b07
Lance Crisang <lancecrisang14@gmail.com>
2023-08-10 01:33:35 +0800

    Update to new site key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiHost: 'https://api.xapier.me/action/api/v1',
-  recaptchaKey: '6Lc5sZInAAAAAGXBr7DejXdRl_dN5gHUz93JzkYW',
+  recaptchaKey: '6LcOxZInAAAAAPBToRb_iDsiwXiwKF5OldfQFqjV',
 };
