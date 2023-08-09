ad377871bbcdd1abd412bb2a2a19447e1d65c8f4
Lance Crisang <lancecrisang14@gmail.com>
2023-08-10 00:35:22 +0800

    Fix incorrect recaptcha site key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiHost: 'https://api.xapier.me/action/api/v1',
-  recaptchaKey: '6Lc5sZInAAAAAKUHD4ZIm7o82DOOH7mDLjm72_ZL',
+  recaptchaKey: '6Lc5sZInAAAAAGXBr7DejXdRl_dN5gHUz93JzkYW',
 };
